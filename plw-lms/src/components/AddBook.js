ce6b46ceda1168f3196cc43180bd0159a42cc7ba
ihplwijesena <ihplwijesena@students.nsbm.ac.lk>
2021-09-28 20:35:30 +0530

    AddBook form button styles

--- plw-lms/src/components/AddBook.js
+++ plw-lms/src/components/AddBook.js
@@ -47,7 +47,7 @@ function AddBook({ onAdd }) {
                     fullWidth
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
-                    style={styles.field}
+                    style={styles.formField}
                     role="new-book-title"
                 />
                 <TextField
@@ -57,7 +57,7 @@ function AddBook({ onAdd }) {
                     fullWidth
                     value={isbn}
                     onChange={(e) => setIsbn(e.target.value)}
-                    style={styles.field}
+                    style={styles.formField}
                     role="new-book-isbn"
                 />
                 <TextField
@@ -67,7 +67,7 @@ function AddBook({ onAdd }) {
                     fullWidth
                     value={author}
                     onChange={(e) => setAuthor(e.target.value)}
-                    style={styles.field}
+                    style={styles.formField}
                     role="new-book-author"
                 />
                 <TextField
@@ -77,7 +77,7 @@ function AddBook({ onAdd }) {
                     fullWidth
                     value={genre}
                     onChange={(e) => setGenre(e.target.value)}
-                    style={styles.field}
+                    style={styles.formField}
                     role="new-book-genre"
                 />
                 <TextField
@@ -87,25 +87,29 @@ function AddBook({ onAdd }) {
                     fullWidth
                     value={publisher}
                     onChange={(e) => setPublisher(e.target.value)}
-                    style={styles.field}
+                    style={styles.formField}
                     role="new-book-publisher"
                 />
-                <Button
-                    type="submit"
-                    color="primary"
-                    variant="outlined"
-                    role="new-book-submit"
-                >
-                    Add Book
-                </Button>
-                <Button
-                    type="reset"
-                    color="secondary"
-                    variant="outlined"
-                    role="new-book-reset"
-                >
-                    Clear
-                </Button>
+                <div style={styles.formButton}>
+                    <Button
+                        type="submit"
+                        color="primary"
+                        variant="outlined"
+                        role="new-book-submit"
+                        style={styles.formButtonAdd}
+                    >
+                        Add Book
+                    </Button>
+                    <Button
+                        type="reset"
+                        color="secondary"
+                        variant="outlined"
+                        role="new-book-clear"
+                        style={styles.formButtonClear}
+                    >
+                        Clear
+                    </Button>
+                </div>
             </form>
 
             <Typography
@@ -124,8 +128,19 @@ function AddBook({ onAdd }) {
 }
 
 const styles = {
-    field: {
+    formField: {
         margin: "0 0 1rem 0"
+    },
+    formButton: {
+        display: "flex"
+    },
+    formButtonAdd: {
+        flex: 4,
+        marginRight: "0.3rem"
+    },
+    formButtonClear: {
+        flex: 1,
+        marginLeft: "0.3rem"
     },
     addedSuccess: {
         color: "blue",
